9403306c7e65408964b7305627c7cb1822bb82b7
gurleen12911 <swt988@gmail.com>
2023-07-08 04:54:23 +0300

    Part 2 Full Stack

---
+++ phonebook/src/App.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react'
+
+const Filter = ({ searchTerm, handleSearchChange }) => {
+  return (
+    <div>
+      filter shown with:
+      <input value={searchTerm} onChange={handleSearchChange} />
+    </div>
+  )
+}
+
+const PersonForm = ({ newName, newNumber, handleNameChange, handleNumberChange, addPerson }) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({ persons, searchTerm }) => {
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+
+  return (
+    <ul>
+      {filteredPersons.map((person) => (
+        <li key={person.id}>
+          {person.name} {person.number}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    // Check if newName already exists in persons array
+    if (persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook`)
+      return
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
+
+      <h3>Add a new</h3>
+
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={persons} searchTerm={searchTerm} />
+    </div>
+  )
+}
+
+export default App
